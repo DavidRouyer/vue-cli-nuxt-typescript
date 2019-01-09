b0dc331fd0bea6a5b0ec40b782bb9d2b10ff678b
David Rouyer <rouyer.david@gmail.com>
2019-01-10 00:34:42 +0100

    chore: cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   /*
-    ** Set source directory
-    */
+   ** Set source directory
+   */
   srcDir: "src/",
   /*
-    ** Headers of the page
-    */
+   ** Headers of the page
+   */
   head: {
     title: "Welcome to Nuxt!",
     meta: [
@@ -13,16 +13,19 @@ module.exports = {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "manifest", href: "/manifest.json" }
+    ]
   },
   modules: ["~/modules/nuxt-typescript"],
   /*
-    ** Customize the progress bar color
-    */
+   ** Customize the progress bar color
+   */
   loading: { color: "#3B8070" },
   /*
-    ** Build configuration
-    */
+   ** Build configuration
+   */
   build: {
     babel: {
       presets: ["@vue/app"]
